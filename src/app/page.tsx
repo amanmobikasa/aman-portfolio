4ee06ab7ff1a0f1e4d557f83fbd5920d94679c74
Aman Chauhan (A-Web Developer) <avinashthakurchauhan07@gmail.com>
2024-06-03 17:55:47 +0530

    adding the landing component text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import EarthGlobe from "./components/EarthGlobe";
 import Navbar from "./components/Navbar";
 import BasicLayout from "./layout/basiclayout";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
@@ -11,8 +12,11 @@ export default function Home() {
         <div className="absolute top-0 left-0 w-full bg-fixed ">
           <Navbar></Navbar>
         </div>
-        <div className="">
-          {/* conntain the left side text */}
+        <div className="absolute left-[3rem] top-0 bottom-0 my-auto w-3/12 h-[100%]  bg-transparent">
+            <div className="p-3 text-balance flex flex-col w-full h-full justify-center">
+                <motion.h2 initial={{opacity : 0, x : -100}} animate={{opacity : 1, x : 0}} transition={{duration : 2, delay : 1.4, ease : "easeInOut", type : "spring", stiffness : 800, damping : 10, }} whileHover={{scale : 1.1, transition : {delay : 0 ,duration : 0.5,  ease : "easeInOut"}}}  className="text-white text-[9rem] leading-tight font-ubuntu font-[600]">Hi, I'm Aman</motion.h2>
+                {/* <p className="text-white">Welcome to my World</p> */}
+            </div>
         </div>
       </BasicLayout>
     </>
